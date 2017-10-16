20b174383e6d51db07799b116c35769306791979
zangaisishi <2779867659@qq.com>
2017-10-16 21:31:45 +0800

    Create main.py

---
+++ py05/main.py
@@ -0,0 +1,7 @@
+import re
+
+
+s= '''我的电子邮件tom@gmail.com.
+         将什么发送到jerry123@163.com或者3123432@qq.com.
+         若遇特殊情况打电话给18212344567.'''
+print(re.findall('[a-zA-Z0-9]+@[a-z0-9]+\.com|\d{11}',s))
